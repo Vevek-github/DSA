6aeeb4095ad021a805225661868da69d1138e9fc
Vevek-github <54910981+Vevek-github@users.noreply.github.com>
2023-12-13 09:05:08 +0530

    Time: 602 ms (92.18%), Space: 23.9 MB (70.12%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -0,0 +1,46 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        
+        return nums
+    
+    
+    
+    # Insertion Sort
+    
+    def sortInsertionArray(self, nums: List[int]) -> List[int]:
+        length =len(nums)
+        for i in range(length):
+            j = i
+            while j < length:
+                if nums[i] > nums[j]:
+                    nums[i],nums[j]= nums[j],nums[i]
+                j+=1
+            
+            
+        return nums
+    
+    # Bubble Sort
+    
+    def sortBubbleArray(slef,nums: List[int])-> List[int]:
+        length = len(nums)
+        for i in range(len(nums)-1,0,-1):
+            for j in range(i):
+                if nums[j]> nums[j+1]:
+                    nums[j+1],nums[j]= nums[j],nums[j+1]
+        return nums
+    
+    # selection Sort 
+    
+    def sortselectionsortArray(self, nums: List[int]) -> List[int]:
+        length =len(nums)
+        for i in range(length):
+            j = i+1
+            min = i
+            while j < length:
+                if nums[j] < nums[min]:
+                    min = j
+                j+=1
+            nums[i],nums[min]= nums[min],nums[i]
+            
+        return nums
