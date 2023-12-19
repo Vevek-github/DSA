691959bbef5bd3220bb9e980a3d8b1f0ace171da
Vevek-github <54910981+Vevek-github@users.noreply.github.com>
2023-12-19 20:55:34 +0530

    Time: 52 ms (6.30%), Space: 16.2 MB (84.30%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,4 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        #nums.sort()
+        n = len(nums)
+        for i in range(len(nums)):
+            j = i+1
+            while j < len(nums):
+                mini = i
+                if nums[mini] < nums[j]:
+                    mini = j
+                nums[mini],nums[j]= nums[j],nums[mini]
+                j+=1
