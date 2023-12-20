1eafd38886c40a73c6a415834eec2c0bc24bab68
Vevek-github <54910981+Vevek-github@users.noreply.github.com>
2023-12-20 11:04:45 +0530

    Time: 151 ms (55.36%), Space: 18 MB (16.52%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,22 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         n =len(nums)
-        hash ={}
-        for i in nums:
-            hash[i]=hash.get(i,0)+1
-        for i in hash:
-            if hash[i]>n//2:
-                return i
-        
+        #hash ={}
+        #for i in nums:
+         #   hash[i]=hash.get(i,0)+1
+        #for i in hash:
+        #    if hash[i]>n//2:
+         #      return i
+        count = 0                      #moore voting algorithim
+        candidate = 0
+        j = 0
+        while j < n:
+            if count == 0:
+                nums[candidate]=nums[j]
+                count=1
+            elif nums[j]==nums[candidate]:
+                count+=1
+            else :
+                count-=1
+            j+=1
+        return nums[candidate]
