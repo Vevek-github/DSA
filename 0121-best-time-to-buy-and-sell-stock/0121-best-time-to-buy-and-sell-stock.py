f65f0ab28825c6c838e2df7da4dc289f6364872a
Vevek-github <54910981+Vevek-github@users.noreply.github.com>
2023-12-22 21:29:54 +0530

    Time: 821 ms (81.68%), Space: 28.8 MB (15.47%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        x = len(prices)
+        if x == 1:
+            return 0
+        left = 0 
+        right = 1
+        maximum = 0 
+        while right < x and left <= right:
+            diff =prices[right] - prices[left]
+            if diff > 0:
+                right +=1
+                
+            if diff<= 0:
+                left=right
+                right+=1
+            maximum = max(maximum ,diff )
+        return maximum
+        
