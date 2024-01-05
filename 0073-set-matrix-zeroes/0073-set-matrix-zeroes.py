8add361e6831dd0694a22facf69df37f6b629480
Vevek-github <54910981+Vevek-github@users.noreply.github.com>
2024-01-05 09:57:42 +0530

    Time: 105 ms (90.72%), Space: 18.2 MB (24.02%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -0,0 +1,25 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        i = 0
+        j =0
+        m = len(matrix)
+        n = len(matrix[0])
+        hor= []
+        ver = []
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j]== 0:
+                    hor.append(i)
+                    ver.append(j)
+        for i in hor:
+            for j in range(n):
+                matrix[i][j]= 0
+        for j in ver:
+            for i in range(m):
+                matrix[i][j]=0
+            
+
+        
